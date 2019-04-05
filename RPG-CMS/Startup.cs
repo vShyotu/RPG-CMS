570e596cc443afd66ed20249b7af08caae274231
Grant Hardman <grant.r.hardman@outlook.com>
2019-04-05 14:48:53 +0100

    Add project files.

---
+++ RPG-CMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RPG_CMS.Startup))]
+namespace RPG_CMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
